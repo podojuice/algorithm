12694e0df5ba654b0574cc103227819870fecb6b
hyungoo <hyungu0611@gmail.com>
2019-08-12 22:46:46 +0900

    0812home

--- java/al/src/bj15686.java
+++ java/al/src/bj15686.java
@@ -11,14 +11,16 @@ public class bj15686 {
 	static int[][] home;
 	static int MIN;
 	static boolean[] used;
+	static int[][] distance;
 	
 	public static void main(String[] args) throws Exception{
 		StringTokenizer st;
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new FileReader(new File("bj15686")));
 		st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		hnum = 0; cnum=0; chicken = new int[13][2]; home = new int[2*N][2]; MIN = 100000000;
+		hnum = 0; cnum=0; chicken = new int[13][2]; home = new int[2*N][2]; MIN = 1000000;
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<N; j++) {
@@ -33,37 +35,49 @@ public class bj15686 {
 				}
 			}
 		}
+		distance = new int[cnum][hnum];
+		
+		for(int i=0; i<cnum; i++) {
+			int cx = chicken[i][0]; int cy = chicken[i][1];
+			for(int j=0; j<hnum; j++) {
+				int hx = home[j][0]; int hy = home[j][1];
+				int x = hx-cx; int y =hy-cy;
+				if(x<0) x = x*-1;
+				if(y<0) y = y*-1;
+				distance[i][j] = x+y;
+			}
+		}
+		
+		
 		used = new boolean[cnum];
-		check(0, M);
+		check(0, M, 0);
 		System.out.println(MIN);
 		
 	}
 	
-	static void check(int cnt, int M) {
+	static void check(int cnt, int M, int asdf) {
 		if(cnt == M) {
 			int sumnum=0;
-			for(int j=0; j<hnum; j++) {
-				int temp = 10000000;
-				for(int i=0; i<cnum; i++) {
-					int hx = home[j][0]; int hy = home[j][1];
-					if(used[i]) {
-						int cx = chicken[i][0]; int cy = chicken[i][1];
-						int x = hx-cx; int y =hy-cy;
-						if(x<0) x = x*-1;
-						if(y<0) y = y*-1;
-						if(x+y<temp) temp = x+y;
+			for(int i=0; i<hnum; i++) {
+				int tmp = 1000000;
+				for(int j=0; j<cnum; j++) {
+					if (used[j]) {
+						if (distance[j][i] < tmp) {
+							tmp = distance[j][i];
+						}
 					}
 				}
-				sumnum += temp;
+				sumnum += tmp;
+				
 			}
 			if(sumnum < MIN) MIN = sumnum;
 			return;
 		}
 		
-		for(int i=cnt; i<cnum; i++) {
+		for(int i=asdf; i<cnum; i++) {
 			if (!used[i]) {
 				used[i] = true;
-				check(cnt+1, M);
+				check(cnt+1, M, i);
 				used[i] = false;
 			}
 		}
